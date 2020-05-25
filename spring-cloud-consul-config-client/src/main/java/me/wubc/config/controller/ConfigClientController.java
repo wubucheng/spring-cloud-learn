c4504ae5219722d651f108297e90a26463fdf429
wbc <wubucheng@timesgroup.cn>
2020-05-25 23:29:58 +0800

    fix:暂存gateway实例代码

--- spring-cloud-consul-config-client/src/main/java/me/wubc/config/controller/ConfigClientController.java
+++ spring-cloud-consul-config-client/src/main/java/me/wubc/config/controller/ConfigClientController.java
@@ -5,6 +5,7 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 /**
  * @author wbc
  * @date 2020/05/25
---
+++ spring-cloud-gateway/src/main/java/me/wubc/gateway/config/GatewayConfig.java
@@ -0,0 +1,32 @@
+package me.wubc.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ConversionServiceFactoryBean;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author wbc
+ * @date 2020/05/25
+ * @desc
+ **/
+@Component
+public class GatewayConfig {
+//    @Bean(name="conversionService")
+//    public ConversionService getConversionService() {
+//        ConversionServiceFactoryBean bean = new ConversionServiceFactoryBean();
+////        bean.setConverters(...); //add converters
+//        bean.afterPropertiesSet();
+//        return bean.getObject();
+//    }
+
+    @Bean
+    public RouteLocator customerRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("path_route2", r -> r.path("/user/getByUsername")
+                        .uri("http://localhost:8085/user/getByUsername"))
+                .build();
+    }
+}
