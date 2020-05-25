4154e799a1cacb68b1e95c5040d030bdc2168f56
wbc <wubucheng@timesgroup.cn>
2020-05-25 21:45:59 +0800

    fix:提交consul实例代码

---
+++ spring-cloud-consule-config-client/src/main/java/me/wubc/config/ConsuleConfigClientApp.java
@@ -0,0 +1,19 @@
+package me.wubc.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author wbc
+ * @date 2020/05/25
+ * @desc
+ **/
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConsuleConfigClientApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsuleConfigClientApp.class, args);
+    }
+}
---
+++ spring-cloud-consule-config-client/src/main/java/me/wubc/config/controller/ConfigClientController.java
@@ -0,0 +1,24 @@
+package me.wubc.config.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wbc
+ * @date 2020/05/25
+ * @desc
+ **/
+@RefreshScope
+@RestController
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/getConfig")
+    public String getConfig() {
+        return configInfo;
+    }
+}
