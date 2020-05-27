9b76301dd3a73ec81dd24d58217078bd18c285e4
wubucheng <wubucheng@timesgroup.cn>
2020-05-27 18:40:34 +0800

    fix: 提交admin示例代码

--- spring-cloud-admin-client/src/main/java/me/wubc/admin/AdminClientApplication.java
+++ spring-cloud-admin-client/src/main/java/me/wubc/admin/AdminClientApplication.java
@@ -2,12 +2,14 @@ package me.wubc.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author wbc
  * @date 2020/05/26
  * @desc
  **/
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AdminClientApplication {
 
--- spring-cloud-admin-server/src/main/java/me/wubc/admin/AdminApplication.java
+++ spring-cloud-admin-server/src/main/java/me/wubc/admin/AdminApplication.java
@@ -3,12 +3,14 @@ package me.wubc.admin;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author wbc
  * @date 2020/05/26
  * @desc
  **/
+@EnableDiscoveryClient
 @EnableAdminServer
 @SpringBootApplication
 public class AdminApplication {
